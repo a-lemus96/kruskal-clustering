743ce3d4a1d2c4ec95043cd51097e585d211d3a2
a-lemus96 <alejandro.lemus@cimat.mx>
2023-05-10 22:49:31 -0500

    feat: Add modified Kruskal's algorithm
    Implement Kruskal's algorithm with termination condition using Weighted
    Quick Union variant of Union-Find algorithm.

--- clustering.py
+++ clustering.py
@@ -3,6 +3,8 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
+# custom modules
+import mst
 
 # load data points from .txt file
 points = [] # to hold coordinate pairs and apply post processing
@@ -21,7 +23,6 @@ points = np.array(points) # cast to numpy array
 diffs = points.reshape(n, 1, 2) - points.reshape(1, n, 2)
 dists = np.sqrt(np.sum(diffs**2, axis=2))
 dists = dists.flatten()
-weights = [{'w': d} for d in dists]
 
 # compute edges
 edges = np.mgrid[0:n, 0:n]
@@ -31,9 +32,11 @@ edges = edges.reshape(-1, 2)
 # create Graph using networkx
 G = nx.Graph()
 G.add_nodes_from(zip(range(n), att)) # add nodes
-G.add_weighted_edges_from(zip(edges[:, 0], edges[:, 1], weights)) # add edges
+G.add_weighted_edges_from(zip(edges[:, 0], edges[:, 1], dists)) # add edges
 G.remove_edges_from(nx.selfloop_edges(G)) # remove self-loops
-print(G)
+
+# compute K-clustering
+mst.kruskal_modified(G)
 # plot points
 #fig, ax = plt.subplots()
 #ax.scatter(points[:, 0], points[:, 1])
---
+++ mst.py
@@ -0,0 +1,60 @@
+# 3rd-party modules
+import networkx as nx
+
+def weighted_quick_union(A, p, q):
+    """Weighted quick-union algorithm.
+    ----------------------------------------------------------------------------"""
+    i = p
+    rank_i = 0
+    # find root for p
+    while A[i] != i:
+       i = A[i]
+       rank_i += 1
+    
+    j = q
+    rank_j = 0
+    # find root for q
+    while A[j] != j:
+        j = A[j]
+        rank_j += 1
+
+    # check if both roots are the same or not
+    if i != j:
+        if rank_i <= rank_j: 
+            A[i] = j
+            old_root = i
+        else:
+            A[j] = i
+            old_root = j
+        return True, old_root
+    else:
+        return False, i
+
+def kruskal_modified(G, k=2):
+    """Kruskal's algorithm with termination condition. User provides the number
+    of desired unconnected components to terminate algorithm.
+    ----------------------------------------------------------------------------
+    Parameters:
+    -----------
+        G: networkx weighted connected graph.
+        k: integer value representing the desired number of components
+    Returns:
+    ---------
+        """
+    edges = G.edges.data() # retrieve edges and their weights
+    edges = list(edges)
+    edges.sort(key=lambda x: x[-1]['weight']) # sort edges by increasing weight
+    edges = iter(edges)
+    T = []
+    # initialize array to implement union-find algorithm
+    n = G.number_of_nodes()
+    A = list(range(n))
+    roots = n * [True] # True if i-th node is a root of a group
+    while sum(roots) > k:
+        e = next(edges)
+        added, rm_idx = weighted_quick_union(A, e[0], e[1])
+        if added:
+            T.append(e) # add edge to MST solution
+            roots[rm_idx] = False # remove node from root nodes
+    print(sum(roots))
+    return T
