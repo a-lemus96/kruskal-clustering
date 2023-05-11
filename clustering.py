91fbdba62ca81cf2acc35e80dbbf366bd0c075a6
a-lemus96 <alejandro.lemus@cimat.mx>
2023-05-10 20:52:01 -0500

    feat: Use networkx module instead of custom one
    Remove `graph.py` file and create graph using networkx interface.
    Read file, compute edges and create graph object.

---
+++ clustering.py
@@ -0,0 +1,40 @@
+# 3rd-party modules
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# load data points from .txt file
+points = [] # to hold coordinate pairs and apply post processing
+att = [] # to hold coordinate pairs as attributes for a graph node
+with open('data.txt', 'r') as f:
+    n = int(f.readline()) # number of data points
+    for k in range(n):
+        pair = f.readline()
+        x, y = pair.split(" ")
+        points.append((float(x), float(y)))
+        att.append({'x': float(x), 'y': float(y)})
+
+points = np.array(points) # cast to numpy array
+
+# compute distances among all points
+diffs = points.reshape(n, 1, 2) - points.reshape(1, n, 2)
+dists = np.sqrt(np.sum(diffs**2, axis=2))
+dists = dists.flatten()
+weights = [{'w': d} for d in dists]
+
+# compute edges
+edges = np.mgrid[0:n, 0:n]
+edges = np.stack((edges[0, ...], edges[1, ...]), axis=-1)
+edges = edges.reshape(-1, 2)
+
+# create Graph using networkx
+G = nx.Graph()
+G.add_nodes_from(zip(range(n), att)) # add nodes
+G.add_weighted_edges_from(zip(edges[:, 0], edges[:, 1], weights)) # add edges
+G.remove_edges_from(nx.selfloop_edges(G)) # remove self-loops
+print(G)
+# plot points
+#fig, ax = plt.subplots()
+#ax.scatter(points[:, 0], points[:, 1])
+#ax.savefig("plot.png")
